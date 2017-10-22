0c9b7166bd4a4aac453bc520e8956441f0f9198f
Ayman Zeine <a.zeine.96@gmail.com>
2017-10-22 17:18:32 -0400

    Part 1 Complete
    Complete Invocation of part 1

--- part1a.c
+++ part1a.c
@@ -11,8 +11,10 @@ void help(char *progname)
 	printf(" to use .so filed)\n");
 }
 
-/* declare the hello() function that lives in a shared library */
+/* declare the hello() and ucase() function that lives in a shared library */
 extern void *hello(void* param);
+
+extern void *ucase(void* param);
 
 /* main() - The main routine parses arguments and invokes hello */
 int main(int argc, char **argv)
@@ -36,6 +38,23 @@ int main(int argc, char **argv)
 	hello(NULL);
 
 	/* TODO: execute the new function "ucase" that you added to libpart1.c */
+	struct team_t *uCase = ucase(&team);
+	
+	printf("Student 1 : %s\n", uCase -> name1);
+    printf("Email 1   : %s\n", uCase -> email1);
+    printf("Student 2 : %s\n", uCase -> name2);
+	printf("Email 2   : %s\n", uCase -> email2);
+	printf("Student 3 : %s\n", uCase -> name3);
+    printf("Email 3   : %s\n", uCase -> email3);
+	printf("\n");
+	
+	free(uCase->name1);
+    free(uCase->email1);
+    free(uCase->name2);
+	free(uCase->email2);
+	free(uCase->name3);
+	free(uCase->email3);
+    free(uCase);
 
-	return 0;
+	exit(0);
 }
