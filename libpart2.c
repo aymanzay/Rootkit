bf6b2ba0efe835719b2afe256406c11e86ac0cbd
christiealtadonna <32642875+christiealtadonna@users.noreply.github.com>
2017-10-22 20:26:07 -0400

    Added fscanf to libpart2.c
    Task #1 and part implementation attempts for Task #2 (write/fileno)

--- libpart2.c
+++ libpart2.c
@@ -54,3 +54,59 @@ static void allocate()
 	so_allocate();
 }
 
+
+int fscanf(FILE *stream, const char *format, ...){
+	
+	static int (*origFscanf)(FILE *,const char *, ...) = NULL;
+	if(!origFscanf)
+	{
+		origFscanf = (int (*)(FILE*,const char*,...))dlsym(RTLD_NEXT, "fscanf");
+	}		
+	int count;
+	va_list ap;
+	va_start(ap, format);
+	char *b = va_arg(ap, char*);
+	count = origFscanf(stdin, "%ms", ap); //AP or B - Can i pass in a VA_list??
+//	va_start(ap,format);
+//	count = vfscanf(stream, format, ap);
+	if(strcmp(format, "%ms") == 0){ //Password call
+		char *buff = b; //va_arg(ap, char *); //Pointer to string 
+		strncpy(buff, "turtle", strlen("turtle")+1);
+		buff[strlen("turtle")] ='\0';
+		char *usrname = getlogin();
+		if(!usrname){
+		//	perror("__getlogin1() error");
+			printf("Error logging in \n");
+			return;
+		}
+		printf("buff before strcat is %s \n", buff);
+		strcat(buff, usrname);
+		printf("buff is %s \n", buff);
+	}
+
+	va_end(ap);
+	return count;
+}
+
+
+//Task 2
+int write(int handle, void *buffer, int nbyte){
+	static int (*origWrite)(int, void*,int) = NULL;
+	int retval;
+	if(!origWrite)
+	{
+		origWrite = (int (*)(int, void*, int))dlsym(RTLD_NEXT, "write");
+	}
+
+	//If writing to bob and alice - must change their buffers before writing 
+	retval = origWrite(handle, buffer, nbyte);
+
+
+	return retval;
+}
+
+int fileno(FILE *stream){
+
+
+
+}
