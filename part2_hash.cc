71e56ac9098962888d7eea062359571f58273782
christiealtadonna <32642875+christiealtadonna@users.noreply.github.com>
2017-10-24 18:37:36 -0400

    updated part2_hash.cc to store data structures needed

--- part2_hash.cc
+++ part2_hash.cc
@@ -13,7 +13,25 @@
 
 using namespace std;
 
+
+struct file{
+	int fd;
+	char * filename;
+};
+
 vector<size_t> *malloc_data = NULL;
+int bobOpen = 0;
+int aliceOpen = 0;
+
+//struct file files[8]; 
+struct file *bobFile = (struct file *)malloc(sizeof(struct file));
+struct file *aliceFile = (struct file *)malloc(sizeof(struct file));
+
+int task = -1;
+
+int prev_value1= 0;
+int prev_value2 =0;
+int transfer_value = 0;
 
 /* declare the following functions to have "C" linkage, so that
  * we can see them from C code without doing name demangling. */
@@ -48,4 +66,86 @@ extern "C"
 		so_allocate();
 		malloc_data->push_back(size);
 	}
+
+
+	void setTask(int t){
+		task = t;
+	}
+
+	int getTask(){
+		return task;
+	}
+
+	void setDesc3(int val){
+		prev_value1 = val;
+	}
+	int getDesc3(){
+		return prev_value1;
+	}
+	void setDesc4(int val){
+		prev_value2 = val;
+	}
+	int getDesc4(){
+		return prev_value2;
+	}
+	void setTransfer(int val){
+		transfer_value =val;
+	}
+	int getTransfer(){
+		return transfer_value;
+	}
+
+	void openBob(int fd,const char *filename){
+		bobOpen =1;
+		bobFile->fd = fd;
+		bobFile->filename = (char *)malloc(sizeof(char)*15);
+		strcpy(bobFile->filename, filename);
+	}
+
+	int getBobFd(){
+		if(bobOpen){
+			return bobFile->fd;
+		}
+		else{
+			return -1;
+		}
+	}
+
+	void closeBob(int fd){
+		if(bobOpen && (bobFile->fd == fd)){
+			bobOpen = 0;
+			free(bobFile->filename);
+			free(bobFile);
+		}
+	}
+
+	void openAlice(int fd,const char *filename){
+		aliceOpen =1;
+		aliceFile->filename = (char *)malloc(sizeof(char)*15);
+		aliceFile->fd = fd;
+		strcpy(aliceFile->filename, filename);
+	}
+
+	void closeAlice(int fd){
+		if(aliceOpen && (aliceFile->fd == fd)){
+			aliceOpen = 0;
+			free(aliceFile->filename);
+			free(aliceFile);
+		}
+	}
+
+	
+	int getAliceFd(){
+		if(aliceOpen){
+			return aliceFile->fd;
+		}
+		else{
+			return -1;
+		}
+	}
+
+
+
+
 }
+
