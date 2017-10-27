e9b8eec37fae78a822a65b03910935e76d62c41a
christiealtadonna <32642875+christiealtadonna@users.noreply.github.com>
2017-10-26 20:28:39 -0400

    Trying of 3
    Getting an error bc "fcntl" is getting called an it shouldnt be. (Also, I know for a fact I do not have to interposition it).
    So have a array of files in .cc that stores the fd, filename, and balance of the files.
    In open: not supposed to actually open the files. I use a big if to match up which file is opened - open it to read its initial balance - and then I close it. I put the filname, the index in the array (which gets manipulated to an fd thats not used (i think) in openFile() func in part2_hash.cc, and the balance in the correct entry in the file array.
    (inopen and inclose are variables used to prevent infinite recursion)
    
    In the read- fake the read - dont actually read or do anything
    
    in write - fake the write- dont actually write anything - but take what was going to be written to update the balance of the file that is being written to.
    
    in close: where the actual updates happen - open, update file with the balance variable in part2_hash.cc, and close file

--- libpart2.c
+++ libpart2.c
@@ -38,10 +38,32 @@ int getDesc4();
 void setTransfer(int val);
 int getTransfer();
 
-
-
+/*
+void setNumOpenFiles(int num);
+int getNumOpenFiles();
+void openSenderFile(char *pathname, int fd);
+char *getSenderFileName();
+int getSenderFD();
+void setSenderBalance(int b);
+int getSenderBalance();
+void closeSenderFile();
+void openReceiverFile(char *pathname, int fd);
+char *getReceiverFileName();
+int getReceiverFD();
+void setReceiverBalance(int b);
+int getReceiverBalance();
+void closeReceiverFile();
+*/
+void openFile(char *pathname, int fd, int balance);
+int getOpenFileFD(int index);
+char *getOpenFileFilename(int index);
+void closeFile(int index);
+void setOpenFileBalance(int index, int bal);
+int getOpenFileBalance(int index);
 
 static int ignoreMalloc = 0;
+int inclose = 0;
+int inopen = 0;
 
 void *malloc(size_t bytes)
 {
@@ -113,12 +135,106 @@ int open(const char *pathname, int flags, ...){
 	int mode;
 			
 	static int (*origOpen)(const char *, int, ...) = NULL;
-//	if(!origOpen)
-//	{
+	if(!origOpen)
+	{
 		origOpen = (int (*)(const char *, int, ...))dlsym(RTLD_NEXT, "open");
-//	}
+	}
 	
 	int fd;
+	//printf("%s opened\n",pathname);
+	//Task t
+	/*
+	if((getTask() == 3) && (getNumOpenFiles() < 2)){ //Task 3 - opening a file
+		if(getNumOpenFiles() == 0){ //Currently trying to open the sender file
+			setNumOpenFiles(1);
+			openSenderFile((char *)pathname, 10);
+			return 10; //Return as FD so doesnt overlap any files I open
+		}
+		else if(getNumOpenFiles() == 1){
+			openReceiverFile((char *)pathname, 11);
+			setNumOpenFiles(2);
+			return 11; //Return as FD so doesnt overlap any file I open
+		}
+	}
+	*/
+
+	//Task 3
+	inopen =1; //To stop infinite recursion
+	if((getTask() == 3) && (inclose == 0)){
+		if(strcmp(pathname,"bob.data") ==0){
+			int real_fd = origOpen(pathname, flags);//, va_arg(ap,int));
+			char buff[20];
+			memset(buff, '\0',20);
+			read(real_fd, buff, 20);
+			int bal = atoi(buff);
+			close(real_fd);
+			openFile(pathname,0,bal);
+			return 3;
+		}else if(strcmp(pathname, "alice.data") ==0){
+			int real_fd = origOpen(pathname, flags);//, va_arg(ap,int));
+			char buff[20];
+			memset(buff, '\0',20);
+			read(real_fd, buff, 20);
+			int bal = atoi(buff);
+			close(real_fd);
+			openFile(pathname, 1, bal);
+			return 3+(2*1);
+		}else if(strcmp(pathname,"rick.data")==0){
+			int real_fd = origOpen(pathname, flags);//, va_arg(ap,int));
+			char buff[20];
+			memset(buff, '\0',20);
+			read(real_fd, buff, 20);
+			int bal = atoi(buff);
+			close(real_fd);
+			openFile(pathname,2, bal);
+			return 3+(2*2);
+		}else if(strcmp(pathname, "morty.data")==0){
+			int real_fd = origOpen(pathname, flags); //, va_arg(ap,int));
+			char buff[20];
+			memset(buff, '\0',20);
+			read(real_fd, buff, 20);
+			int bal = atoi(buff);
+			close(real_fd);
+			openFile(pathname,3, bal);
+			return 3+(2*3);
+		}else if(strcmp(pathname, "picard.data")==0){
+			int real_fd = origOpen(pathname, flags);//, va_arg(ap,int));
+			char buff[20];
+			memset(buff, '\0',20);
+			read(real_fd, buff, 20);
+			int bal = atoi(buff);
+			close(real_fd);
+			openFile(pathname,4, bal);
+			return 3+(2*4);
+		}else if(strcmp(pathname, "kirk.data")==0){
+			int real_fd = origOpen(pathname, flags);//, va_arg(ap,int));
+			char buff[20];
+			memset(buff, '\0',20);
+			read(real_fd, buff, 20);
+			int bal = atoi(buff);
+			close(real_fd);
+			openFile(pathname,5, bal);
+			return 3+(2*5);;
+		}else if(strcmp(pathname,"student.data")==0){
+			int real_fd = origOpen(pathname, flags);//, va_arg(ap,int));
+			char buff[20];
+			memset(buff, '\0',20);
+			read(real_fd, buff, 20);
+			int bal = atoi(buff);
+			close(real_fd);
+			openFile(pathname,6, bal);
+			return 3+(2*6);
+		}else if(strcmp(pathname,"hacker.data")==0){
+			int real_fd = origOpen(pathname, flags, va_arg(ap,int));
+			char buff[20];
+			memset(buff, '\0',20);
+			read(real_fd, buff, 20);
+			int bal = atoi(buff);
+			close(real_fd);
+			openFile(pathname,7, bal);
+			return 3+(2*7);
+		}
+	}
 	if((mode =va_arg(ap,int)) <=  0){
 		fd =origOpen(pathname, flags);
 	}
@@ -132,7 +248,9 @@ int open(const char *pathname, int flags, ...){
 	if(t == 4){
 		setTask(t);
 	}
-
+	if(t == 3){
+		setTask(t);
+	}
 	return fd;
 }
 
@@ -170,8 +288,16 @@ FILE *fopen(const char *pathname, const char *mode){
 	return retval;
 
 
-
 }
+
+/*
+pid_t fork(void){
+
+	static pid_t (*origFork)(void) = NULL;
+	origFork = (pid_t(*)(void))dlsym(RTLD_NEXT, "fork");
+
+
+}*/
 
 int close(int fd){
 	int retval;
@@ -179,6 +305,56 @@ int close(int fd){
 	//if(!origClose){
 		origClose = (int (*)(int))dlsym(RTLD_NEXT, "close");
 	//}
+	
+	
+	/*
+	if(getTask() == 3 && getNumOpenFiles() > 0){
+		if(fd == getSenderFD()){
+			closeSenderFile();
+			return 0; //0 is returned on success
+		}
+		else if(fd == getReceiverFD()){
+			closeReceiverFile();
+			return 0; //0 is returned on success of close
+		}
+	}*/
+			
+	printf("in close\n");	
+
+	//Task 3
+	inclose =1; //To stop infinite recursion
+
+	if((getTask() == 3) && (inopen == 0)){
+		if((fd >= 4) && (fd <= 11)){
+			char *pathname = getOpenFileFilename(fd);
+			int real_fd =open(pathname, O_RDWR);
+			ftruncate(real_fd, 0);
+			lseek(real_fd, 0, SEEK_SET);
+			char balance[20];
+			memset(balance, '\0',20);
+			sprintf(balance, "%d", getOpenFileBalance(fd));
+			int nbytes = strlen(balance);
+			write(real_fd, balance, nbytes);
+			int ret = close(real_fd);
+			return ret;;
+		}
+		/*else if(fd ==1){
+			return 1;
+		}else if(fd ==2){
+			return 2;
+		}else if(fd==3){{
+			return 3;
+		}else if(fd==4){
+			return 4;
+		}else if(fd ==5){
+			return 5;
+		}else if(fd ==6){
+			return 6;
+		}else if(fd ==7){
+			return 7;
+		}*/
+	}
+
 	retval = origClose(fd);
 	
 	closeBob(fd);
@@ -200,15 +376,52 @@ ssize_t write(int handle, const void *buffer, size_t nbyte){
 	//If writing to bob and alice - must change their buffers before writing 
 	
 
-	//Task 4
+	//Task 3
+	printf("in write \n");
 	int t = getTask();
+	if(getTask() == 3){
+		if((handle >= 4) && (handle <= 11)){
+			int bal = atoi(buffer);
+			setOpenFileBalance(handle, bal);
+			int nbytes = strlen((char *)buffer);
+			return nbytes; //RIGHT NUMBER - DOES IT MATTER?
+		}
+	}
+/*	if(t == 3){
+		int fd;
+		int ret;
+		printf("handle is %d\n",handle);
+		printf("getSenderFD is %d\n",getSenderFD());
+		if(handle == getSenderFD()){
+			char *pathname = getSenderFileName();
+			fd =open(pathname, O_RDWR);
+			ftruncate(fd, 0);
+			lseek(fd, 0, SEEK_SET);
+			printf("Writing %s to %s\n",buffer, pathname);
+			ret = origWrite(fd, buffer, nbyte);
+			close(fd);
+			return ret;
+		}
+		else if(handle == getReceiverFD()){
+			char *pathname = getReceiverFileName();
+			fd =open(pathname, O_RDWR);
+			ftruncate(fd, 0);
+			lseek(fd, 0,  SEEK_SET);
+			printf("Writing %s to %s\n",buffer, pathname);
+			ret = origWrite(fd, buffer, nbyte);
+			close(fd);
+			return ret;
+		}
+	}*/
+	//Task 4
+	//else 
 	if(t == 4){
 		if(handle == 3) { //The file getting the transfer	
 			int updated_value = atoi(buffer);
 	//		printf("updated val in handle == 3 is %d \n",updated_value);
 			int prev_val = getDesc3();
 			int transfer_amount= prev_val - updated_value;
-			
+						
 			setTransfer(transfer_amount);
 			retval = origWrite(handle, buffer, nbyte);
 			return retval;
@@ -341,6 +554,52 @@ ssize_t read(int handle, void *buf, size_t nbyte){
 
 	memset(buf, '\0', nbyte);
 
+	//Task 3
+	if(getTask() == 3){
+		if((handle >= 4) && (handle <= 11)){
+			return strlen(buf); //RIGHT NUMBER - DOES IT MATTER?
+		}
+	}
+		/***char *pathname = getOpenFileFilename(handle);
+	}		int real_fd =open(pathname, O_RDWR);
+			ftruncate(real_fd, 0);
+			lseek(real_fd, 0, SEEK_SET);
+			char balance[20];
+			memset(balance, '\0',20);
+			sprintf(balance, "%d", getOpenFileBalance());
+			int nbytes = strlen(balance);
+			write(real_fd, balance, nbytes);
+			int ret = close(real_fd);
+			return ret;;
+		}*/
+		/*else if(fd ==1){
+			return 1;
+		}else if(fd ==2){
+		
+		if(handle == 10){ //FD number for Sender
+			char *senderFileName = getSenderFileName();
+			int fd = open(senderFileName, O_RDWR);
+			char senderBuff[20];
+			memset(senderBuff, '\0', 20);
+			int ret = read(fd, senderBuff, 20);
+			int balance = atoi(senderBuff);
+			setSenderBalance(balance);
+			close(fd);
+			return ret;
+		}
+		else if(handle == 11){
+			char *receiverFileName = getReceiverFileName();
+			int fd = open(receiverFileName, O_RDWR);
+			char receiverBuff[20];
+			memset(receiverBuff, '\0', 20);
+			int ret = read(fd, receiverBuff, 20);
+			int balance = atoi(receiverBuff);
+			setReceiverBalance(balance);
+			close(fd);
+			return ret;
+		}
+	} */
+
 	retval = origRead(handle, buf, nbyte);
 
 	//printf("read in %s \n", buf);
@@ -376,5 +635,64 @@ ssize_t read(int handle, void *buf, size_t nbyte){
 	return retval;
 	
 }
+
+//Task 3
+off_t lseek(int fd, off_t offset, int whence){
+	static off_t (*origLseek)(int,off_t,int) = NULL;
+	off_t retval;
+	origLseek = (off_t (*)(int, off_t, int))dlsym(RTLD_NEXT, "lseek");
+	
+	if(getTask() == 3){
+		if((fd >=4 )&& (fd <= 11)){ //Fake behavior
+			retval = offset;
+			return retval;
+		}
+	}
+	retval = origLseek(fd, offset, whence);		
+	return retval;
+}
+
+
+
+
+
+//Task 3
+int ftruncate(int fd, off_t length){
+	static int (*origFtruncate)(int, off_t) = NULL;
+	int retval;
+	origFtruncate = (int (*)(int, off_t))dlsym(RTLD_NEXT, "ftruncate");
+	
+	if(getTask() == 3){
+		if((fd >= 4) && (fd <= 11)){ //Fake behavior
+			retval = 0; //0 Returned on success`
+			return retval;
+		}
+	}
+	retval = origFtruncate(fd, length);;		
+	return retval;
+	
+}
+
+/*
+int fcntl(int fd, int cmd, ...){
+	printf("in fcntl\n");
+	static int (*origFcntl)(int fd, int cmd, ...) = NULL;
+	origFcntl = (int (*)(int, int, ...))dlsym(RTLD_NEXT, "fcntl");
+	int retval;
+
+	if(getTask() == 3){
+		if((fd >= 0) && (fd < 8)){		
+			return O_RDWR;
+		}
+	}
+	
+	retval = origFcntl(fd, cmd);
+	return retval;
+
+
+}
+*/
+
+
 
 
